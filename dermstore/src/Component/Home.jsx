e0dd1a4be13a260d36bf6a11f9c8051762eaed97
HirenShudra <shudrahiren8@gmail.com>
2022-09-28 21:52:41 +0530

    changes added

---
+++ dermstore/src/Component/Home.jsx
@@ -0,0 +1,13 @@
+// import Navbar  from "./Navbar";
+import Header from './Header';
+
+
+export default function Home(){
+    return(
+        <div>
+            
+            <Header />
+            
+        </div>
+    )
+}
