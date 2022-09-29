69d25aee082b715821f0cf16ac83a51c23641e5d
HirenShudra <shudrahiren8@gmail.com>
2022-09-29 21:54:41 +0530

    changes added to project

--- dermstore/src/App.js
+++ dermstore/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-import Navbar from './Component/Navbar';
+import Home from '../Component/Home';
+import Navbar from '../Component/Navbar';
+import Header from '../Component/Header';
+import Allroutes from '../Pages/Allroutes';
+
 
 function App() {
   return (
     <div className="App"> 
-      <Navbar />
-      
+        <Header />
+        <Home />
+        <Navbar />    
+        <Allroutes />    
     </div>
   );
 }
