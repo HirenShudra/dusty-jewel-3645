b511deb8f1d4efce5237007ae538706b80d95fdf
HirenShudra <shudrahiren8@gmail.com>
2022-10-02 12:49:27 +0530

    chnages added

--- dermstore/src/Component/Login.jsx
+++ dermstore/src/Component/Login.jsx
@@ -1,9 +1,21 @@
+import '../App.css';
 import React from "react";
+ 
 
 export default function Login(){
     return (
-        <>
-        <h1>Login</h1>
-        </>
+    <div className="login">
+        <div>
+            <h1>Existing Customers</h1>
+            <div><label><span>*</span>Email address <br /> <input type="email" /></label></div>
+            <div><label><span>*</span>Password <br /> <input type="password" /></label></div>
+            <div><button>Login</button></div>
+        </div>
+        <div>
+            <h1>New Customers</h1>
+            <button>CONTINUE</button>
+        </div>
+    </div>
+    
     )
 }
